dd2a5703d72d26009f3066d17d99cff0a809d90a
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-09-11 15:32:19 +0700

    update Router.php eksekusi controller dari router

--- app/App/Router.php
+++ app/App/Router.php
@@ -25,7 +25,9 @@
       {
         if($path == $route['path'] && $method == $route['method'])
         {
-          echo "CONTROLLER : " . $route['controller'] . ", FUNCTION : " . $route['function'];
+          $controller = new $route['controller'];
+          $function = $route['function'];
+          $controller->$function();
           return;
         }
       }
