3b5f767349dfc3ebfeba80f7f258ef121e7c9f86
mrosyidi <rosyidiyuekunchen@gmail.com>
2024-09-12 09:05:08 +0700

    update Router.php mengubah router menggunakan regex

--- app/App/Router.php
+++ app/App/Router.php
@@ -23,7 +23,8 @@
       $method = $_SERVER['REQUEST_METHOD'];
       foreach(self::$routes as $route)
       {
-        if($path == $route['path'] && $method == $route['method'])
+        $pattern = '#^' . $route['path'] . '$#';
+        if(preg_match($pattern, $path, $variables) && $method == $route['method'])
         {
           $controller = new $route['controller'];
           $function = $route['function'];
